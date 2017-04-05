ec2c4be9b341f524c8fe0c133cb07130a9e23c26
Othesius <othesius.exe@gmail.com>
2017-04-05 08:53:34 -0400

    Basic layout setup. Working on arrayadapter

--- app/src/main/java/com/example/android/newsapp/Article.java
+++ app/src/main/java/com/example/android/newsapp/Article.java
@@ -1,8 +1,88 @@
 package com.example.android.newsapp;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Class to build a news article object
  */
 
-public class Article {
+public class Article implements Parcelable {
+
+    // Class Variables
+    private int mImage;
+
+    private String mTitle;
+
+    private String mPublisher;
+
+    private long mDate;
+
+    // Class constructor
+    private void Article(int image, String title, String publisher, long date) {
+        mImage = image;
+        mTitle = title;
+        mPublisher = publisher;
+        mDate = date;
+    }
+
+    // Class getter methods for individual attributes
+
+    public int getImage() {
+        return mImage;
+    }
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getPublisher() {
+        return mPublisher;
+    }
+
+    public long getDate() {
+        return mDate;
+    }
+
+    // Formatted string that returns the title, date and publisher
+    @Override
+    public String toString() {
+        return "Title: " + mTitle +
+                "Published on: " + mDate +
+                "For: " + mPublisher;
+    }
+
+    //TODO Implement Parcelable
+    public Article(Parcel in) {
+        mPublisher = in.readString();
+        mDate = in.readLong();
+        mPublisher = in.readString();
+        mImage = in.readInt();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mPublisher);
+        dest.writeString(mTitle);
+        dest.writeLong(mDate);
+        dest.writeInt(mImage);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Article> CREATOR = new Parcelable.Creator<Article>() {
+        @Override
+        public Article createFromParcel(Parcel in) {
+            return new Article(in);
+        }
+
+        @Override
+        public Article[] newArray(int size) {
+            return new Article[size];
+        }
+    };
 }
+
--- app/src/main/java/com/example/android/newsapp/ArticleAdapter.java
+++ app/src/main/java/com/example/android/newsapp/ArticleAdapter.java
@@ -1,10 +1,63 @@
 package com.example.android.newsapp;
 
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
- * Created by Othesius on 3/30/17.
+ * Custom adapter class to handle news articles
  */
 
 public class ArticleAdapter extends ArrayAdapter<Article> {
+
+    // Constructor for Article Adapter.
+    public ArticleAdapter(Activity context, ArrayList<Article> articles) {
+        super(context, 0, articles);
+    }
+
+    // Method for finding a view to recycle.
+    // If no view is found method inflates a new one.
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // If no view can be recycled, inflate a new one.
+        if (convertView == null) {
+
+        }
+        // Gets the item's position in the list
+        Article thisArticle = getItem(position);
+
+        // Assign the views to be used in the layout
+        ImageView articleImageView = (ImageView) convertView.findViewById(R.id.article_image);
+        TextView publisherView = (TextView) convertView.findViewById(R.id.publisher);
+        TextView dateView = (TextView) convertView.findViewById(R.id.publish_date);
+        TextView titleView = (TextView) convertView.findViewById(R.id.title);
+
+        // Format the date for display
+        Date dateObject = new Date(thisArticle.getDate());
+        String formattedDate = formatDate(dateObject);
+
+
+        // Call the class getter methods to set the relevant information on each view
+        articleImageView.setImageResource(thisArticle.getImage());
+        publisherView.setText(thisArticle.getPublisher());
+        dateView.setText(formattedDate);
+
+        return convertView;
+    }
+
+    private String formatDate(Date dateObject) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("LLL dd, yyyy");
+        return dateFormat.format(dateObject);
+
+    }
 }
